28d2fddb2ca3b9b0beea80f1c4e6988f356968e4
Abhi-9738 <74170591+Abhi-9738@users.noreply.github.com>
2022-02-16 11:15:14 +0530

    Fourth commit- Added page and test result folders and modified the test_demo1 file

--- test_script/test_demo1.py
+++ test_script/test_demo1.py
@@ -1,1 +1,11 @@
-print("HI..")
+from generic.base_test import BaseTest
+from generic.excel import Excel
+
+class Test_Demo1(BaseTest):
+
+
+    def test_demo1(self):
+         #print("demo1")
+         print(self.driver.title)
+         v=Excel.get_data(self.xl_path,"Sheet1",1,1)
+         print("from excel file: ",v)
